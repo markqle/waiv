d77c909d3a58f8763c0ddfc122f3c2c5e43143ab
SA1 <cdc.workability-sa1@csulb.edu>
2025-08-20 15:45:54 -0700

    collect static

--- cdcwaiv/cdcwaiv/settings/production.py
+++ cdcwaiv/cdcwaiv/settings/production.py
@@ -11,3 +11,13 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['.yourdomain.com'])
 DATABASES = {
     'default': env.db('DATABASE_URL')
 }
+
+#HTTPS settings -> This redirects HTTP traffic to HTTPS
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+#HSTS settings
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
